e8ec4ed53cf8b4e0755ad1aa0035a8347244228d
Andres Long <andreslong95@gmail.com>
2020-03-28 00:44:30 -0500

    some ES6 structure changes

--- public/db.js
+++ public/db.js
@@ -1,14 +1,26 @@
 let db;
 const request = window.indexedDB.open("Budget", 1);
 
-request.onupgradeneeded = function(event) {
+request.onupgradeneeded = event => {
     db = event.target.result;
     const pendingStore = db.createObjectStore("pending", { autoIncrement: true });
     pendingStore.createIndex("statusIndex", "status");
 };
 
+request.onsuccess = event => {
+    db = event.target.result;
+  
+    if (navigator.onLine) {
+      checkDatabase();
+    }
+};
 
-request.onerror = function(event) {
-    // log error here
-    console.log(event);
-  };
+request.onerror = event => {console.log(event)};
+
+function saveRecord(record) {
+    db = request.result;
+    const transaction = db.transaction(["pending"], "readwrite");
+    const transpending = transaction.objectStore("pending");
+  
+    transpending.add(record);
+}
