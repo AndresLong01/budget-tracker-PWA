26f17c22da0bc82ca3dbd4e10bd014d3ff7ba77e
Andres Long <andreslong95@gmail.com>
2020-03-28 00:42:48 -0500

    some chanegs to the request for the database. Added error handling

--- public/db.js
+++ public/db.js
@@ -1,2 +1,14 @@
 let db;
 const request = window.indexedDB.open("Budget", 1);
+
+request.onupgradeneeded = function(event) {
+    db = event.target.result;
+    const pendingStore = db.createObjectStore("pending", { autoIncrement: true });
+    pendingStore.createIndex("statusIndex", "status");
+};
+
+
+request.onerror = function(event) {
+    // log error here
+    console.log(event);
+  };
