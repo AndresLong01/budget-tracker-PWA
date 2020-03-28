919a0634393f7bcf2934673c3a5e1eccdf12d90c
Andres Long <andreslong95@gmail.com>
2020-03-28 00:46:09 -0500

    added functionality to the check database function

--- public/db.js
+++ public/db.js
@@ -24,3 +24,29 @@ function saveRecord(record) {
   
     transpending.add(record);
 }
+
+function checkDatabase() {
+    const checkTransaction = db.transaction(["pending"], "readwrite");
+    const checking = checkTransaction.objectStore("pending");
+    const getAll = checking.getAll();
+    getAll.onsuccess = function() {
+      if (getAll.result.length > 0) {
+        fetch("/api/transaction/bulk", {
+          method: "POST",
+          body: JSON.stringify(getAll.result),
+          headers: {
+            Accept: "application/json, text/plain, */*",
+            "Content-Type": "application/json"
+          }
+        })
+        .then(response => response.json())
+        .then(() => {
+            const succTransaction = db.transaction(["pending"], "readwrite");
+            const success = succTransaction.objectStore("pending");
+            success.clear();
+        });
+      }
+    };
+}
+
+window.addEventListener("online", checkDatabase);
